7fc623ee93bd618f4ca0d398df39355b3fb07980
Web-Code <ahmedwahaj317@gmail.com>
2025-05-26 17:00:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -28,3 +28,7 @@ const saveNotes = () => {
 //  <div class="note">
 // <div class="tool">
 //     <i class="fas fa-save"></i>
+//     <i class="fas fa-trash"></i>
+// </div>
+// <textarea></textarea>
+ // </div>

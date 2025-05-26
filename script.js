eacd9fe15c68797dc034655802579d9efd697d1e
Web-Code <ahmedwahaj317@gmail.com>
2025-05-26 16:57:50 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 
 const addBtn = document.querySelector("#addBtn")
 const main = document.querySelector("#main")
+addBtn.addEventListener(
+    "click",
+    function() {
+        addNote()
+    }
+)
